ada585f8799081065c425ee97a508e86c1d4d15f
HanaTarek <105328136+HanaTarek@users.noreply.github.com>
2023-09-15 16:02:38 +0300

    first submit

---
+++ src/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Card = () => {
+  return (
+    <div className="card">
+      {/* Add card content here */}
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/components/TodoForm.js
@@ -0,0 +1,23 @@
+import React, {useState} from "react";
+
+export const TodoForm = ({addTodo}) =>{
+
+    const [ Value, setValue] = useState('');
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+
+        addTodo(Value);
+
+        setValue("")
+     
+    }
+    return(
+        <form onSubmit={handleSubmit} className="TodoForm" >
+           <input type="text" value={Value}onChange={(e) => setValue(e.target.value)}
+             className="todo-input" placeholder="What is the task today?"
+/>
+           <button type="submit" className="todo-btn">Add Task</button>
+        </form>
+    )
+}
